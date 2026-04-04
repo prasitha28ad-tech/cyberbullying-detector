7efc14e8e6c88d11d93b2838541365a90cb09882
Prasi <prasitha.28ad@licet.ac.in>
2026-04-04 11:15:31 +0530

    fixed port issue

--- app.py
+++ app.py
@@ -169,4 +169,6 @@ def logout():
 # RUN
 # ---------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
